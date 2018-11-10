348771303cdb3e4ca27b63d4209c0b2324ffbbaa
DanielDonato <daniel_donato10@yahoo.com.br>
2018-11-10 14:34:48 -0200

    Cadastro_Acitivity

--- app/src/main/java/com/example/danieldonato/whatsappclone/CadastroActivity.java
+++ app/src/main/java/com/example/danieldonato/whatsappclone/CadastroActivity.java
@@ -5,17 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class CadastroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_cadastro);
     }
 
-    public void abrirTelaCastro(View view){
-        Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
-        startActivity(intent);
-    }
+
 
 }

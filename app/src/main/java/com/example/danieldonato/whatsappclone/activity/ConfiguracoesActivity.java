953aa58e38a3bce5d01cf50cdfe6b2319131d7d3
DanielDonato <daniel_donato10@yahoo.com.br>
2018-12-02 18:53:43 -0200

    Abrindo a galeria

--- app/src/main/java/com/example/danieldonato/whatsappclone/activity/ConfiguracoesActivity.java
+++ app/src/main/java/com/example/danieldonato/whatsappclone/activity/ConfiguracoesActivity.java
@@ -55,6 +55,17 @@ public class ConfiguracoesActivity extends AppCompatActivity {
             }
         });
 
+        imageButtonGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //abre a galeria
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if(i.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(i, SELECAO_GALERIA);
+                }
+            }
+        });
+
     }
 
     @Override

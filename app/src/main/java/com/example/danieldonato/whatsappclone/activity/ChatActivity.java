b5486c4bf2624c3d77d132a1ba825e09c21471f6
DanielDonato <daniel_donato10@yahoo.com.br>
2019-05-05 17:21:59 -0300

    Salving mensagem

--- app/src/main/java/com/example/danieldonato/whatsappclone/activity/ChatActivity.java
+++ app/src/main/java/com/example/danieldonato/whatsappclone/activity/ChatActivity.java
@@ -1,24 +1,97 @@
 package com.example.danieldonato.whatsappclone.activity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.danieldonato.whatsappclone.R;
+import com.example.danieldonato.whatsappclone.config.ConfiguracaoFirebase;
+import com.example.danieldonato.whatsappclone.helper.Base64Custom;
+import com.example.danieldonato.whatsappclone.helper.UsuarioFirebase;
+import com.example.danieldonato.whatsappclone.model.Mensagem;
+import com.example.danieldonato.whatsappclone.model.Usuario;
+import com.google.firebase.database.DatabaseReference;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ChatActivity extends AppCompatActivity {
+
+    private TextView textViewnNome;
+    private CircleImageView circleImageViewFoto;
+    private Usuario usuarioDestinatario;
+    private EditText editMensagem;
+
+    //identificador usuarios remetente e destinatario
+    private String idUsuarioRemetente;
+    private String idUsuarioDestinatario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         toolbar.setTitle("");
+        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //configuracoes iniciais
+        textViewnNome = findViewById(R.id.textViewNomeChat);
+        circleImageViewFoto = findViewById(R.id.circleImageFotoChat);
+        editMensagem = findViewById(R.id.editMensagem);
+
+        //recuperar os dados do usuario remetente
+        idUsuarioRemetente = UsuarioFirebase.getIdentificadorUsuario();
+
+        //recuperar os dados do usuario destinatario
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null){
+
+            usuarioDestinatario = (Usuario) bundle.getSerializable("chatContato");
+            textViewnNome.setText(usuarioDestinatario.getNome());
+            String foto = usuarioDestinatario.getFoto();
+            if(foto != null){
+                Uri url = Uri.parse(usuarioDestinatario.getFoto());
+                Glide.with(ChatActivity.this)
+                        .load(url)
+                        .into(circleImageViewFoto);
+            }else{
+                circleImageViewFoto.setImageResource(R.drawable.padrao);
+            }
+            //recuperar dados do usuario destinatario
+            idUsuarioDestinatario = Base64Custom.codificarBase64(usuarioDestinatario.getEmail());
+        }
+    }
+
+    public void enviarMensagem(View view){
+        String textoMensagem = editMensagem.getText().toString();
+        if(!textoMensagem.isEmpty()){
+            Mensagem mensagem = new Mensagem();
+            mensagem.setIdUsuario(idUsuarioRemetente);
+            mensagem.setMensagem(textoMensagem);
+            salvarMensagem(idUsuarioRemetente, idUsuarioDestinatario, mensagem);
+            editMensagem.setText("");
+        }else {
+            Toast.makeText(ChatActivity.this,
+                    "Digite uma menagem para enviar!",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void salvarMensagem(String idRemetente, String idDestinatario, Mensagem msg){
+        DatabaseReference database = ConfiguracaoFirebase.getFirebaseDatabase();
+        DatabaseReference mensagemRef = database.child("mensagens");
+        mensagemRef.child(idRemetente)
+            .child(idDestinatario)
+            .push()
+            .setValue(msg);
     }
 
 }

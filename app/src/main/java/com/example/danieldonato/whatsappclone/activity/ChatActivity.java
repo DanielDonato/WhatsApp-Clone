e31dedc810757b4adc76b8c08ea94f1e4bd0debf
DanielDonato <daniel_donato10@yahoo.com.br>
2019-05-05 18:22:08 -0300

    Listing mensages

--- app/src/main/java/com/example/danieldonato/whatsappclone/activity/ChatActivity.java
+++ app/src/main/java/com/example/danieldonato/whatsappclone/activity/ChatActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
@@ -13,12 +15,19 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.danieldonato.whatsappclone.R;
+import com.example.danieldonato.whatsappclone.adapter.MensagensAdapter;
 import com.example.danieldonato.whatsappclone.config.ConfiguracaoFirebase;
 import com.example.danieldonato.whatsappclone.helper.Base64Custom;
 import com.example.danieldonato.whatsappclone.helper.UsuarioFirebase;
 import com.example.danieldonato.whatsappclone.model.Mensagem;
 import com.example.danieldonato.whatsappclone.model.Usuario;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -28,10 +37,18 @@ public class ChatActivity extends AppCompatActivity {
     private CircleImageView circleImageViewFoto;
     private Usuario usuarioDestinatario;
     private EditText editMensagem;
+    private DatabaseReference database;
+    private DatabaseReference mensagensRef;
+    private ChildEventListener childEventListenerMensagens;
 
     //identificador usuarios remetente e destinatario
     private String idUsuarioRemetente;
     private String idUsuarioDestinatario;
+
+    private RecyclerView recyclerMensagens;
+    private MensagensAdapter adapter;
+    private List<Mensagem> mensagens = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +63,7 @@ public class ChatActivity extends AppCompatActivity {
         textViewnNome = findViewById(R.id.textViewNomeChat);
         circleImageViewFoto = findViewById(R.id.circleImageFotoChat);
         editMensagem = findViewById(R.id.editMensagem);
+        recyclerMensagens = findViewById(R.id.recyclerMensagem);
 
         //recuperar os dados do usuario remetente
         idUsuarioRemetente = UsuarioFirebase.getIdentificadorUsuario();
@@ -68,6 +86,21 @@ public class ChatActivity extends AppCompatActivity {
             //recuperar dados do usuario destinatario
             idUsuarioDestinatario = Base64Custom.codificarBase64(usuarioDestinatario.getEmail());
         }
+
+        //configurar adapter
+        adapter = new MensagensAdapter(mensagens, getApplicationContext());
+
+        //configuracao recyclerview
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerMensagens.setLayoutManager(layoutManager);
+        recyclerMensagens.setHasFixedSize(true);
+        recyclerMensagens.setAdapter(adapter);
+
+        //configuração firebase
+        database = ConfiguracaoFirebase.getFirebaseDatabase();
+        mensagensRef = database.child("mensagens")
+                .child(idUsuarioRemetente)
+                .child(idUsuarioDestinatario);
     }
 
     public void enviarMensagem(View view){
@@ -92,6 +125,51 @@ public class ChatActivity extends AppCompatActivity {
             .child(idDestinatario)
             .push()
             .setValue(msg);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        recuperarMensagens();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mensagensRef.removeEventListener(childEventListenerMensagens);
+    }
+
+    private void recuperarMensagens(){
+
+        childEventListenerMensagens = mensagensRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Mensagem mensagem = dataSnapshot.getValue(Mensagem.class);
+                mensagens.add(mensagem);
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
 }
---
+++ app/src/main/java/com/example/danieldonato/whatsappclone/adapter/MensagensAdapter.java
@@ -0,0 +1,87 @@
+package com.example.danieldonato.whatsappclone.adapter;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.danieldonato.whatsappclone.R;
+import com.example.danieldonato.whatsappclone.helper.UsuarioFirebase;
+import com.example.danieldonato.whatsappclone.model.Mensagem;
+
+import java.net.URI;
+import java.util.List;
+
+public class MensagensAdapter extends RecyclerView.Adapter<MensagensAdapter.MyViewHolder> {
+
+    private List<Mensagem> mensagens;
+    private Context context;
+    private static final int TIPO_REMETENTE = 0;
+    private static final int TIPO_DESTINATARIO = 1;
+
+    public MensagensAdapter(List<Mensagem> lista, Context c) {
+        this.mensagens = lista;
+        this.context = c;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View item = null;
+        if(viewType == TIPO_REMETENTE){
+            item = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.adapter_mensagem_remetente, viewGroup, false);
+        }else if(viewType == TIPO_DESTINATARIO){
+            item = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.adapter_mensagem_destinatario, viewGroup, false);
+        }
+        return new MyViewHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
+        Mensagem mensagem = mensagens.get(i);
+        String msg = mensagem.getMensagem();
+        String img = mensagem.getImagem();
+        if(img != null){
+            Uri url = Uri.parse(img);
+            Glide.with(context).load(url).into(myViewHolder.imagem);
+            //esconder o texto
+            myViewHolder.mensagem.setVisibility(View.GONE);
+        }else{
+            myViewHolder.mensagem.setText(msg);
+            //esconder imagem
+            myViewHolder.imagem.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mensagens.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Mensagem mensagem = mensagens.get(position);
+        String idUsuario = UsuarioFirebase.getIdentificadorUsuario();
+        if(idUsuario.equals(mensagem.getIdUsuario())){
+            return TIPO_REMETENTE;
+        }
+        return TIPO_DESTINATARIO;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView mensagem;
+        ImageView imagem;
+        public MyViewHolder(View view){
+            super(view);
+            mensagem = view.findViewById(R.id.textMensagemText);
+            imagem = view.findViewById(R.id.imageMensagemFoto);
+        }
+
+    }
+}
